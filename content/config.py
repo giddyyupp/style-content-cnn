4b923202aae0f0cb9ed0bf6a35d2d67dfc5677da
Samet Hi <giddyyupp@gmail.com>
2020-12-21 19:21:46 +0300

    Update config.py

--- content/config.py
+++ content/config.py
@@ -28,7 +28,7 @@ train_path = train_dir_path + "/"
 
 # test data
 test_dir_path = '../dataset_content/test/'
-test_path = test_dir_path + 'mbrown/'
+test_path = test_dir_path + 'mbrown/' # you may need to change this illustrator
 
 # test model name
 MODEL_NAME = "../models/content_model-99.pkl"
